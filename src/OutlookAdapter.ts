002a9a9744443e24d23c6b66f25622752d32cb99
Benjamin Kluck <kluck@sipgate.de>
2019-06-19 11:10:57 +0200

    chore: Lint, format and typescript config

--- src/OutlookAdapter.ts
+++ src/OutlookAdapter.ts
@@ -1,64 +1,36 @@
-import {Adapter, Config, Contact, PhoneNumberLabel} from "@clinq/bridge";
-import {Request} from "express";
-import {OutlookContact} from "./model";
+import { Adapter, Config, Contact, PhoneNumberLabel } from "@clinq/bridge";
+import { Client } from "@microsoft/microsoft-graph-client";
+import { Request } from "express";
+import { parseEnvironment } from "./config";
+import { OutlookContact } from "./model";
 
+const { APP_ID, APP_PASSWORD, APP_SCOPES, REDIRECT_URI } = parseEnvironment();
 
 const credentials = {
 	client: {
-		id: process.env.APP_ID,
-		secret: process.env.APP_PASSWORD,
+		id: APP_ID,
+		secret: APP_PASSWORD
 	},
 	auth: {
-		tokenHost: 'https://login.microsoftonline.com',
-		authorizePath: 'common/oauth2/v2.0/authorize',
-		tokenPath: 'common/oauth2/v2.0/token'
+		tokenHost: "https://login.microsoftonline.com",
+		authorizePath: "common/oauth2/v2.0/authorize",
+		tokenPath: "common/oauth2/v2.0/token"
 	}
 };
 
 export class OutlookAdapter implements Adapter {
-
-	private toClinqContact(contacts: OutlookContact[]): Contact[] {
-		return contacts.map(contact => ({
-			id: contact.id,
-			name: contact.displayName,
-			firstName: contact.givenName,
-			lastName: contact.surname,
-			email: contact.emailAddresses.find(i => true).address,
-			organization: contact.companyName,
-			phoneNumbers: [
-				...contact.homePhones.map(phoneNumber => ({
-					label: PhoneNumberLabel.HOME,
-					phoneNumber: phoneNumber
-				})),
-				...contact.businessPhones.map(phoneNumber => ({
-					label: PhoneNumberLabel.WORK,
-					phoneNumber: phoneNumber
-				})),
-				...contact.mobilePhone ? ([{
-					label: PhoneNumberLabel.MOBILE,
-					phoneNumber: contact.mobilePhone
-				}]) : []
-			],
-			contactUrl: "",
-			avatarUrl: ""
-		}));
-	}
-
 	public async getContacts(config: Config): Promise<Contact[]> {
-
-		const graph = require('@microsoft/microsoft-graph-client');
-
-		const client = graph.Client.init({
-			authProvider: (done) => {
+		const client = Client.init({
+			authProvider: done => {
 				done(null, config.apiKey.split(":")[0]);
 			}
 		});
 
 		const outlookContacts = await client
-		.api('/me/contacts')
-		.select('id,givenName,surname,emailAddresses,companyName,displayName,businessPhones,homePhones,mobilePhone')
-		.orderby('givenName ASC')
-		.get();
+			.api("/me/contacts")
+			.select("id,givenName,surname,emailAddresses,companyName,displayName,businessPhones,homePhones,mobilePhone")
+			.orderby("givenName ASC")
+			.get();
 
 		return outlookContacts ? this.toClinqContact(outlookContacts.value) : [];
 	}
@@ -66,33 +38,65 @@ export class OutlookAdapter implements Adapter {
 	public async getOAuth2RedirectUrl(): Promise<string> {
 		const host = credentials.auth.tokenHost;
 		const path = credentials.auth.authorizePath;
-		const scopes = process.env.APP_SCOPES.split(" ").join("+");
-		const callbackUri = encodeURI(process.env.REDIRECT_URI);
-		const appId = process.env.APP_ID;
-		return `${host}/${path}?redirect_uri=${callbackUri}&scope=${scopes}&response_type=code&client_id=${appId}`;
-	};
+		const scopes = APP_SCOPES.split(" ").join("+");
+		const callbackUri = encodeURI(REDIRECT_URI);
+		return `${host}/${path}?redirect_uri=${callbackUri}&scope=${scopes}&response_type=code&client_id=${APP_ID}`;
+	}
 
 	public async handleOAuth2Callback(req: Request): Promise<Config> {
+		const { code } = req.query;
 
-		const {code} = req.query;
-
-		const oauth2Client = require('simple-oauth2').create(credentials);
+		const oauth2Client = require("simple-oauth2").create(credentials);
 
 		const result = await oauth2Client.authorizationCode.getToken({
-			client_id: process.env.APP_ID,
-			client_secret: process.env.APP_PASSWORD,
-			code: code,
-			redirect_uri: process.env.REDIRECT_URI,
-			scope: process.env.APP_SCOPES
+			client_id: APP_ID,
+			client_secret: APP_PASSWORD,
+			code,
+			redirect_uri: REDIRECT_URI,
+			scope: APP_SCOPES
 		});
 
-		const {access_token: accessToken, id_token: idToken} = oauth2Client.accessToken.create(result).token;
+		const { access_token: accessToken, id_token: idToken } = oauth2Client.accessToken.create(result).token;
 
 		const config = {
 			apiKey: `${accessToken}:`,
-			apiUrl: "",
+			apiUrl: ""
 		};
 
 		return config;
-	};
+	}
+
+	private toClinqContact(contacts: OutlookContact[]): Contact[] {
+		return contacts.map(contact => {
+			const email = contact.emailAddresses.find(Boolean);
+			return {
+				id: contact.id,
+				name: contact.displayName,
+				firstName: contact.givenName,
+				lastName: contact.surname,
+				email: email ? email.address : null,
+				organization: contact.companyName,
+				phoneNumbers: [
+					...contact.homePhones.map(phoneNumber => ({
+						label: PhoneNumberLabel.HOME,
+						phoneNumber
+					})),
+					...contact.businessPhones.map(phoneNumber => ({
+						label: PhoneNumberLabel.WORK,
+						phoneNumber
+					})),
+					...(contact.mobilePhone
+						? [
+								{
+									label: PhoneNumberLabel.MOBILE,
+									phoneNumber: contact.mobilePhone
+								}
+						  ]
+						: [])
+				],
+				contactUrl: "",
+				avatarUrl: ""
+			};
+		});
+	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { start } from "@clinq/bridge";
+import { config } from "dotenv";
 import { OutlookAdapter } from "./OutlookAdapter";
+
+config();
 
 start(new OutlookAdapter());
